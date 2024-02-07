0fd258c8e8ca1bc1951a253ccbe9310e48cef7e1
Gabe L <gabe@fijiwebdesign.com>
2024-02-07 12:18:00 +0300

    res type

--- lib/esm/screengrab.d.ts
+++ lib/esm/screengrab.d.ts
@@ -1,13 +1,16 @@
-type ScreenGrabClientProps = {
+export type ScreenGrabClientProps = {
     server: string;
     apiKey: string;
+};
+export type ScreenGrabResponse = {
+    url: string;
+    innerHTML: string;
 };
 export declare class ScreenGrabClient {
     siteUrl: string;
     opts: ScreenGrabClientProps;
     constructor(opts?: Partial<ScreenGrabClientProps>);
     url(siteUrl: string): this;
-    grab(selector?: string): Promise<any>;
+    grab(selector?: string): Promise<ScreenGrabResponse>;
 }
 export declare const ScreenGrab: (opts?: Partial<ScreenGrabClientProps> | string) => ScreenGrabClient;
-export {};
